109f81dd8520a96c6275964eae75df4885a48a7c
nanpuyue <nanpuyue@gmail.com>
2021-08-09 09:08:22 +0800

    chore: trap signal earlier, pass extra parameters to openvpn

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+kill_and_wait() {
+    kill "${PIDS[@]}" 2>&-
+    wait "${PIDS[@]}"
+}
 
 UPDOWN="/etc/openvpn/update-resolv-conf"
 LOCAL="$(ip --json addr show eth0 | jq --raw-output '.[0].addr_info[0].local')"
@@ -8,9 +12,10 @@ ip route add table 200 $(ip route show default)
 ip rule add from "$LOCAL" lookup 200
 
 openvpn --script-security 2 --up "$UPDOWN" --down "$UPDOWN" --auth-nocache\
-    --cd "$PWD" --config "$OVPN" &
+    --cd "$PWD" --config "$OVPN" "$@" &
 PIDS[${#PIDS[@]}]="$!"
 
+trap kill_and_wait EXIT
 
 while kill -0 "${PIDS[0]}" 2>&-; do
     sleep 1
@@ -21,11 +26,4 @@ echo "start sock5s"
 sock5s -l "$LOCAL:1080" &
 PIDS[${#PIDS[@]}]="$!"
 
-
-kill_and_wait() {
-    kill "${PIDS[@]}" 2>&-
-    wait "${PIDS[@]}"
-}
-
-trap kill_and_wait EXIT
 wait -n "${PIDS[@]}"
